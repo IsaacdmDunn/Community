4a5024964ed4f371c30ee6ad19757caf81a13988
IsaacDunn <55945831+IsaacDunn@users.noreply.github.com>
2020-07-14 16:53:28 +0100

    V0.12 Basic network capability
    Added basic networking using Unity's UNet system also added functionality for players to host servers and join as a client with their own player object assigned to them if they join

---
+++ Project Community/Assets/Scripts/PlayerNetworkInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerNetworkInfo : NetworkBehaviour
+{
+    public GameObject PlayerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //check if this is player's own object
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+        
+        //command server to spawn player
+        CmdSpawnUnit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [Command]
+    void CmdSpawnUnit() 
+    {
+        //give player object and send to all clients
+        GameObject go = Instantiate(PlayerPrefab);
+        NetworkServer.Spawn(go);
+    }
+}
